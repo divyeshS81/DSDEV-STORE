43367542bbc6d67e8c387208916cc7f3d5849791
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-05-09 18:52:04 +0530

    add footer section setup complate

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,66 @@
+import React from "react";
 import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
+
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer className="foote">
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">About</div>
+          <div className="text">
+            Voluptatem accusantimdoloremque laudantim, totam rem aperiam, eaque
+            ipsa quae ab illo inventore veritatis et quasi architecto beatac
+            vitae dicta explicabo eaque ipsa quae ab illo.
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Contact</div>
+          <div className="c-item">
+            <FaLocationArrow />
+            <div className="text">
+              150ft Ring Road, shital park, rajkot-360007
+            </div>
+          </div>
+          <div className="c-item">
+            <FaMobileAlt />
+            <div className="text">Phone: +91-8140184084</div>
+          </div>
+          <div className="c-item">
+            <FaEnvelope />
+            <div className="text">Email: divyeshsarvaiya999@gmail.com</div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Categories</div>
+          <span className="text">Headphones</span>
+          <span className="text">Smart Watches</span>
+          <span className="text">Bluetooth Speakers</span>
+          <span className="text">Wireless Earbuds</span>
+          <span className="text">Home Theatre</span>
+          <span className="text">Projectors</span>
+        </div>
+        <div className="col">
+          <div className="title">Pages</div>
+          <span className="text">Home</span>
+          <span className="text">About</span>
+          <span className="text">Privacy Policy</span>
+          <span className="text">Returns</span>
+          <span className="text">Terms & Conditions</span>
+          <span className="text">Contact Us</span>
+        </div>
+      </div>
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+            <div className="text">
+                DSDEVSTORE 2024 CREATED BY DS DEV. PREMIUM E_COMMERCE SOLUTIONS.
+            </div>
+            <img src={Payment} alt="Payments" />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
