f85255f6dcaabb9f646e79df2b4cbda2c892638e
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-05-10 18:25:24 +0530

    Shopping Cart section done

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -2,11 +2,25 @@ import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 
 import "./Cart.scss";
-const Cart = () => {
+const Cart = ({setShowCart}) => {
     return (
         <div className="cart-panel">
             <div className="opac-layer"></div>
-            <div className="cart-content">avdf</div>
+            <div className="cart-content">
+                <div className="cart-header">
+                    <span className="heading">Shopping Cart</span>
+                    <span className="close-btn" onClick={() => {setShowCart(false)}}>
+                        <MdClose />
+                        <span className="text">Close</span>
+                    </span>
+                </div>
+                
+                <div className="empty-cart">
+                    <BsCartX /> 
+                    <span>No Products In The Cart.</span>
+                    <button className="return-cta">RETURN TO SHOP</button>                   
+                </div>
+            </div>
         </div>
     );
 };
