2d493d56a7ef063542e9e3cb9e9debc295939ef6
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-05-10 18:55:29 +0530

    Shopping Cart component is done in this commit

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 
+import CartItem from "./CartItem/CartItem";
+
 import "./Cart.scss";
 const Cart = ({setShowCart}) => {
     return (
@@ -15,11 +17,24 @@ const Cart = ({setShowCart}) => {
                     </span>
                 </div>
                 
-                <div className="empty-cart">
+                {/* <div className="empty-cart">
                     <BsCartX /> 
                     <span>No Products In The Cart.</span>
                     <button className="return-cta">RETURN TO SHOP</button>                   
-                </div>
+                </div> */}
+
+                <>
+                    <CartItem />
+                    <div className="cart-footer">
+                        <div className="subtotal">
+                            <span className="text">Subtotal:</span>
+                            <span className="text total">&#8377;1234</span>
+                        </div>
+                        <div className="button">
+                            <button className="checkout-cta">Checkout</button>
+                        </div>
+                    </div>
+                </>
             </div>
         </div>
     );
--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,31 @@
+import { MdClose } from "react-icons/md"
 import "./CartItem.scss";
+import prod from "../../../assets/products/earbuds-prod-1.webp"
+
 const CartItem = () => {
-    return <div>Cart item</div>;
+    return (
+        <div className="cart-products">
+            <div className="cart-product">
+                <div className="img-container">
+                    <img src={prod} alt="" />
+                </div>
+                <div className="prod-details">
+                    <span className="name">Product Name</span>
+                    <MdClose  className="close-btn"/>
+                    <div className="quantity-buttons">
+                        <span>-</span>
+                        <span>5</span>
+                        <span>+</span>
+                    </div>
+                    <div className="text">
+                        <span>3</span>
+                        <span>x</span>
+                        <span>&#8377;4567</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CartItem;
